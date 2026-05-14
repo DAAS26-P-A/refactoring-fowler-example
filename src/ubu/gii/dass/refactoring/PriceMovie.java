dff6a9c50de7f1e5f399908da04ffe20466271b5
Dankof04 <dmm1017@alu.ubu.es>
2026-05-14 16:03:06 +0200

    #1 Creación de métodos abstractos 'getCharge()' y 'getFrequentRenterPoints()' en la superclase

--- src/ubu/gii/dass/refactoring/PriceMovie.java
+++ src/ubu/gii/dass/refactoring/PriceMovie.java
@@ -3,4 +3,8 @@ package ubu.gii.dass.refactoring;
 abstract class PriceMovie {
 
 	abstract int getPriceCode();
+	
+	abstract double getCharge(int _daysRented);
+	
+	abstract int getFrequentRenterPoints(int _daysRented);
 }
